a43712a550e8030ba0387f31870b5c94af3aadad
Kinetic639 <michal.stepien36@gmail.com>
2022-01-23 17:59:20 +0100

    added update, getOne, and listAll methods to ChilRecord

--- records/child.record.js
+++ records/child.record.js
@@ -1,22 +1,59 @@
+const { pool } = require('../utils/db');
+const { ValidationError } = require('../utils/errors');
+const { v4: uuid } = require('uuid');
+
 class ChildRecord {
-  static listAll() {
-    return [
+  constructor(obj) {
+    if (!obj.name || obj.name.length < 3 || obj.name.length > 25) {
+      throw new ValidationError('Imię musi mieć od 3 do 25 znaków...');
+    }
+
+    this.id = obj.id;
+    this.name = obj.name;
+  }
+
+  async insert() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+
+    await pool.execute(
+      'INSERT INTO `children`(`id`,`name`) VALUES(:id, :name)',
       {
-        id: 'abcd',
-        name: 'Ania',
-        gift: 'Lalka',
+        id: this.id,
+        name: this.name,
       },
+    );
+
+    return this.id;
+  }
+
+  static async getOne(id) {
+    const [results] = await pool.execute(
+      'SELECT * FROM `children` WHERE `id` = :id',
       {
-        id: 'aeed',
-        name: 'Krzyś',
-        gift: 'Samochodzik',
+        id,
       },
+    );
+    return results.length === 0 ? null : new ChildRecord(results[0]);
+  }
+
+  async update(id) {
+    await pool.execute(
+      'UPDATE `children`SET `name` = :name, `giftId` = :giftId WHERE `id` = :id ',
       {
-        id: 'aqqd',
-        name: 'Piotruś',
-        gift: 'Klocki',
+        id: this.id,
+        name: this.name,
+        giftId: this.giftId,
       },
-    ];
+    );
+  }
+
+  static async listAll() {
+    const [result] = await pool.execute(
+      'SELECT * FROM `children` ORDER BY `name` ASC',
+    );
+    return result;
   }
 }
 
