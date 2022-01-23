78a52ee3871234045a68331adec03a45027a4966
Kinetic639 <michal.stepien36@gmail.com>
2022-01-23 17:59:46 +0100

    added post and patch requests to child router

--- routers/child.js
+++ routers/child.js
@@ -1,17 +1,42 @@
 const { Router } = require('express');
 const { ChildRecord } = require('../records/child.record');
 const { GiftRecord } = require('../records/gift.record');
-
+const { ValidationError } = require('../utils/errors');
 const childRouter = Router();
 
-childRouter.get('/', (req, res) => {
-  const childrenList = ChildRecord.listAll();
-  const giftsList = GiftRecord.listAll();
-  res.render('children/list', {
-    childrenList,
-    giftsList,
+childRouter
+
+  .get('/', async (req, res) => {
+    const childrenList = await ChildRecord.listAll();
+    const giftsList = await GiftRecord.listAll();
+    res.render('children/list', {
+      childrenList,
+      giftsList,
+    });
+  })
+
+  .post('/', async (req, res) => {
+    const newChild = new ChildRecord(req.body);
+    await newChild.insert();
+
+    res.redirect('/child');
+  })
+
+  .patch('/gift/:childId', async (req, res) => {
+    const child = await ChildRecord.getOne(req.params.childId);
+    if (child === null) {
+      throw new ValidationError('Nie znaleziono dziecka o podanym ID...');
+    }
+    const gift =
+      req.params.giftId === ''
+        ? null
+        : await GiftRecord.getOne(req.body.giftId);
+
+    child.giftId = gift?.id ?? null;
+    await child.update();
+
+    res.redirect('/child');
   });
-});
 
 module.exports = {
   childRouter,
