21979d374f0c19e0a65454be32b980601bfa5bd2
Kinetic639 <michal.stepien36@gmail.com>
2022-04-28 10:38:17 +0200

    switching to typescript

---
+++ utils/db.ts
@@ -0,0 +1,11 @@
+import {createPool} from 'mysql2/promise';
+const dotenv = require('dotenv').config();
+
+export const pool = createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+    namedPlaceholders: true,
+    decimalNumbers: true,
+});
