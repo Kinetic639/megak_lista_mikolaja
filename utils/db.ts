188380cf99d8c5b3ab184d976c806da233205600
Kinetic639 <michal.stepien36@gmail.com>
2022-04-28 10:56:49 +0200

    added .env file

--- utils/db.ts
+++ utils/db.ts
@@ -1,5 +1,7 @@
 import {createPool} from 'mysql2/promise';
-const dotenv = require('dotenv').config();
+import {config} from "dotenv";
+
+const dotenv = config();
 
 export const pool = createPool({
   host: process.env.DB_HOST,
