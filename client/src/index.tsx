1c4893b9db6d8f4a75357e6a29fbc8833d442beb
Kinetic639 <michal.stepien36@gmail.com>
2022-05-11 19:12:56 +0200

    config for heroku

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import './index.css';
 import 'normalize.css';
 import reportWebVitals from './reportWebVitals';
@@ -7,12 +7,14 @@ import {App} from "./App";
 import {BrowserRouter as Router} from 'react-router-dom'
 import CssBaseline from "@mui/material/CssBaseline";
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = ReactDOM.createRoot(rootElement);
+root.render(
         <Router>
             <CssBaseline/>
             <App/>
-        </Router>,
-    document.getElementById('root'),
+        </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- server/index.ts
+++ server/index.ts
@@ -1,18 +1,19 @@
-import * as express from "express";
+import * as express from 'express';
 import * as cors from 'cors';
 import 'express-async-errors';
-import {giftRouter} from "./routers/gift";
-import {childRouter} from "./routers/child";
+import { giftRouter } from './routers/gift';
+import { childRouter } from './routers/child';
 
-import {handleError} from "./utils/errors";
+import { handleError } from './utils/errors';
 import './utils/db';
-
 
 const app = express();
 
-app.use(cors({
-    origin: 'https://santas-gifts.netlify.app/gifts',
-}));
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  }),
+);
 app.use(express.json()); // Content-type: application/json
 
 app.use('/children', childRouter);
@@ -22,8 +23,8 @@ app.use(handleError);
 const port = Number(process.env.PORT) || 3001;
 
 app.get('/', (req, res) => {
-    res.send(`santas gifts app is running ${port}`)
-})
+  res.send(`santas gifts app is running ${port}`);
+});
 app.listen(port, '0.0.0.0', () => {
-    console.log(`Listening on http://localhost:${port}`);
+  console.log(`Listening on http://localhost:${port}`);
 });
