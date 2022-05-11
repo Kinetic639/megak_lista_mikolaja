cbc0f0f580ed696a05172849fa4a3716f246a8a1
Kinetic639 <michal.stepien36@gmail.com>
2022-05-11 22:09:49 +0200

    setting up config

--- server/index.ts
+++ server/index.ts
@@ -11,7 +11,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'https://santas-gifts.netlify.app',
+    // origin: 'https://santas-gifts.netlify.app',
+    origin: 'http://localhost:3000',
   }),
 );
 app.use(express.json()); // Content-type: application/json
