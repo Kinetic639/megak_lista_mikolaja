0b3bdcf9f77651da77463bab33b00c229b5dfb54
Kinetic639 <michal.stepien36@gmail.com>
2022-05-11 19:16:47 +0200

    changinf cors

--- server/index.ts
+++ server/index.ts
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://santas-gifts.netlify.app',
   }),
 );
 app.use(express.json()); // Content-type: application/json
