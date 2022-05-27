5bd06e535d186d64c1588227c555049dc140a037
Kinetic639 <michal.stepien36@gmail.com>
2022-05-27 12:04:21 +0200

    change favicon and title, added _target=blank to link in information view

--- client/src/views/InformationView/InformationView.tsx
+++ client/src/views/InformationView/InformationView.tsx
@@ -8,17 +8,17 @@ export const InformationView = () => {
    
     return <Box>       
         <Typography paragraph>
-           For more information please visit this project's github: <Link href="https://bit.ly/3MoI8L4" underline="hover">
+           For more information please visit this project's github: <Link href="https://bit.ly/3MoI8L4" target='_blank' underline="hover">
   {'megak_lista_mikołaja'}
 </Link>
         </Typography>
         <Typography paragraph>
-           My github page: <Link href="https://bit.ly/3FpbAxS" underline="hover">
+           My github page: <Link target='_blank' href="https://bit.ly/3FpbAxS" underline="hover">
   {'https://github.com/Kinetic639'}
 </Link>
         </Typography>
         <Typography paragraph>
-           My portfolio website: <Link href="https://bit.ly/3KOPTsg" underline="hover">
+           My portfolio website: <Link target='_blank' href="https://bit.ly/3KOPTsg" underline="hover">
   {'www.michal-stepien.com'}
 </Link>
         </Typography>
